c494d61f879715be0146729a0c3759e99b998b22
Damian-DOM <damian.krakow@gmail.com>
2011-11-26 15:42:00 +0100

    initial commit

---
+++ example.php
@@ -0,0 +1,36 @@
+<?php
+//MySQL connection parameters
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpsw = '';
+$dbname = '';
+
+//Connects to mysql server
+$connessione = @mysql_connect($dbhost,$dbuser,$dbpsw);
+mysql_query("SET CHARSET utf8");
+mysql_query("SET NAMES 'utf8' COLLATE 'utf8_bin'");
+
+//Includes class
+require_once('MySQLDump.php');
+
+//Creates a new instance of MySQLDump: it exports a compressed and base-16 file
+$dumper = new MySQLDump($dbname,'filename.sql',false,false);
+
+//Use this for plain text and not compressed file
+//$dumper = new MySQLDump($dbname,'filename.sql',false,false);
+
+//Dumps all the database
+$dumper->doDump();
+  
+//Dumps all the database structure only (no data)
+//$dumper->getDatabaseStructure();
+
+//Dumps all the database data only (no structure)
+//$dumper->getDatabaseData();
+
+//Dumps "mytable" table structure only (no data)
+//$dumper->getTableStructure('mytable');
+
+//Dumps "mytable" table data only (no structure)
+//$dumper->getTableData('mytable');
+?>
